070f0ef153356fc65308e3caa747dfa14c82879e
TRIF3X <thewizardofcrypto@gmail.com>
2019-03-04 14:02:50 -0600

    initialize linked list and node class

--- queue/queue.py
+++ queue/queue.py
@@ -1,11 +1,34 @@
 # Monday
 
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+
+class Linked_list:
+  def __init__(self, head, tail, length):
+    self.head = head
+    self.tail = tail
+    self.length = length
+
+    def add_to_tail(self, value):
+      pass
+
+    def add_to_head(self, value):
+      pass
+
+    def remove_head():
+      pass
+
+    def remove_tail():
+      pass
+
 class Queue:
   def __init__(self):
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = 
+    self.storage = []
 
   def enqueue(self, item):
     pass
