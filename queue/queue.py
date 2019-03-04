724642b7724ef9ce9df8402153b8e8bdfd486e84
TRIF3X <thewizardofcrypto@gmail.com>
2019-03-04 15:13:26 -0600

    linked list complete

--- queue/queue.py
+++ queue/queue.py
@@ -11,53 +11,60 @@ class Linked_list:
     self.tail = tail
     self.length = 0
 
-    def add_to_tail(self, value):
-      n = Node(value)
+  def add_to_tail(self, value):
+    n = Node(value)
+    if not self.head:
+      self.head = n
+      self.tail = n
+    else:
       self.tail.next_node = n
       self.tail = n 
-      self.length += 1
+    self.length += 1
 
 
-    def add_to_head(self, value):
-      n = Node(value)
+  def add_to_head(self, value):
+    n = Node(value)
+    if not self.head:
+      self.head = n
+      self.tail = n
+    else:
       n.next_value = self.head
       self.head = n
-      self.length += 1
+    self.length += 1
 
-    def remove_head(self):
-      if not self.head:
-        return None
-      if self.length == 1:
-        self.head = None
-        self.tail = None
-        self.length -= 1
-        return self.head
-      else:
-        current_head = self.head
-        self.head = self.head.next_node
-        self.length -= 1
-        return current_head
+  def remove_head(self):
+    if not self.head:
+      return None
+    if self.length == 1:
+      self.head = None
+      self.tail = None
+      self.length -= 1
+      return self.head
+    else:
+      current_head = self.head
+      self.head = self.head.next_node
+      self.length -= 1
+      return current_head
 
 
-    def remove_tail(self):
-      # if our length is less than 2 we don't have a head or a tail just a node
-      if not self.tail:
-        return None
-      if self.length == 1:
-        self.head = None
-        self.tail = None
-        self.length -= 1
-        return self.tail
-      else:
-        current_node = self.head
-        previous_node = None
-        while current_node is not None:
-          previous_node = current_node
-          current_node = current_node.next_node
-        else:
-          previous_node.next_node = None
-          self.length -= 1
-
+  def remove_tail(self):
+    # if our length is less than 2 we don't have a head or a tail just a node
+    if not self.tail:
+      return None
+    if self.length == 1:
+      self.head = None
+      self.tail = None
+      self.length -= 1
+      return self.tail
+    else:
+      current_node = self.head
+      previous_node = None
+      while current_node is not None:
+        previous_node = current_node
+        current_node = current_node.next_node
+      previous_node.next_node = None
+      self.tail = previous_node
+      self.length -= 1
 
 
 class Queue:
