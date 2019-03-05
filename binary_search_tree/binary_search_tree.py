230198483d466c830f5b3ce8a96299e9421dfd6f
TRIF3X <thewizardofcrypto@gmail.com>
2019-03-05 14:17:15 -0600

    insert complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -7,11 +7,18 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    bst = BinarySearchTree(value)
-    if bst.left and bst.right is None:
-      root = bst
+    if self.left and self.right and self.value is None:
+      self.value = value
+    elif value < self.value:
+      if self.left is None:
+        self.left = BinarySearchTree(value)
+      else:
+        self.left.insert(value)
     else:
-      while bst.left or bst.right is not None:
+      if self.right is None:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
 
 
   def contains(self, target):
@@ -19,6 +26,17 @@ class BinarySearchTree:
 
   def get_max(self):
     # traverse to the right until we can no longer move right, if we hit None we know the node above it is the max
-    while self.right != None:
+    while self.right is not None:
       self.right
-    return self.value
+    if self.right == None:
+      return self.value
+
+
+t = BinarySearchTree(30)
+t.insert(20)
+t.insert(25)
+t.insert(50)
+t.insert(77)
+t.insert(92)
+print(t.get_max())
+
