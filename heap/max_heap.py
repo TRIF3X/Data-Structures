c3761acde31d29a2b757bd2afe6e91486dbc4d28
TRIF3X <thewizardofcrypto@gmail.com>
2019-03-06 14:12:47 -0600

    revise bubble_up

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,18 +1,17 @@
 # Wednesday
 
 class Heap:
-  def __init__(self, parent):
+  def __init__(self):
     self.storage = []
 
   def insert(self, value):
     self.storage.append(value)
-    self._bubble_up()
+    self._bubble_up(len(self.storage) -1)
 
   def delete(self):
     self.storage.insert(0, self.storage.pop())
     self.storage.pop(1)
-    self._sift_down()
-
+    self._sift_down(0)
 
   def get_max(self):
     # If stored in an array, our first value is the 'root' which means it is the biggest value
@@ -23,12 +22,12 @@ class Heap:
     return len(self.storage)
 
   def _bubble_up(self, index):
-    left_child = (index * 2) + 1
-    right_child = (index * 2) + 2
     parent = (index - 1) // 2
 
     if self.storage[parent] < self.storage[index]:
-      self.storage[parent], self.storage[index] = self.storage[index], self.storage[parent]
+        self.storage[parent], self.storage[index] = self.storage[index], self.storage[parent]
+        self._bubble_up(parent)
+
 
   def _sift_down(self, index):
     left_child = (index * 2) + 1
