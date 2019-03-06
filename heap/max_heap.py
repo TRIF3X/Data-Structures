a1c5d891560d3db37c1a6926a6fa549c511b084b
TRIF3X <thewizardofcrypto@gmail.com>
2019-03-06 13:40:19 -0600

    sift down and bubble up

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -15,13 +15,28 @@ class Heap:
     return self.storage[0]
 
   def get_size(self):
-    pass
+    # return the length of the storage
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+    left_child = (index * 2) + 1
+    right_child = (index * 2) + 2
+    parent = (index - 1) // 2
+
+    if self.storage[parent] < self.storage[index]:
+      self.storage[parent], self.storage[index] = self.storage[index], self.storage[parent]
 
   def _sift_down(self, index):
-    pass
+    left_child = (index * 2) + 1
+    right_child = (index * 2) + 2
+    parent = (index - 1) // 2
+    # if the left node is greater than the right node, check left node against parent node. If child node has higher value swap them
+    if self.storage[left_child] > self.storage[right_child]:
+      if self.storage[left_child] > self.storage[parent]:
+        self.storage[left_child], self.storage[parent] = self.storage[parent], self.storage[left_child]
+    elif self.storage[right_child] > self.storage[left_child]:
+      if self.storage[right_child] > self.storage[parent]:
+        self.storage[right_child], self.storage[parent] = self.storage[parent], self.storage[right_child]
 
 
 
